289e6113ff1f437fe336215acaacc58899b0ec2b
rassafel <rassafel@gmail.com>
2022-02-24 17:43:03 +0400

    Add Person mapper.

--- spring-boot-demo/src/main/java/ru/nshi/demo/config/OrikaConfig.java
+++ spring-boot-demo/src/main/java/ru/nshi/demo/config/OrikaConfig.java
@@ -1,10 +1,13 @@
 package ru.nshi.demo.config;
 
-import ma.glasnost.orika.DefaultFieldMapper;
+import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.nshi.demo.model.domain.Person;
+import ru.nshi.demo.model.dto.CreatePersonDto;
 
 /**
  * @author rassafel
@@ -14,6 +17,19 @@ public class OrikaConfig {
     @Bean
     MapperFactory mapperFactory() {
         DefaultMapperFactory factory = new DefaultMapperFactory.Builder().build();
+        factory.classMap(CreatePersonDto.class, Person.class)
+            .field("rawAge", "age")
+            .customize(new CustomMapper<CreatePersonDto, Person>() {
+                @Override
+                public void mapAtoB(CreatePersonDto source, Person target, MappingContext context) {
+                    String[] values = source.getFio().split("\\s+");
+                    target.setLastName(values[0]);
+                    target.setFirstName(values[1]);
+                    target.setMiddleName(values[2]);
+                }
+            })
+            .byDefault()
+            .register();
         return factory;
     }
 }
