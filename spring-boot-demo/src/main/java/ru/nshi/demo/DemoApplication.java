e67bc8777dc23ab465268a05a27f2585d210db7b
rassafel <rassafel@gmail.com>
2022-02-24 17:34:50 +0400

    Add Spring Application class and context test.

---
+++ spring-boot-demo/src/main/java/ru/nshi/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package ru.nshi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rassafel
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ spring-boot-demo/src/test/java/ru/nshi/demo/DemoApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.nshi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author rassafel
+ */
+@SpringBootTest
+public class DemoApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
