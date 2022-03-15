d9f444d731de5180b0507c56a0f8ef707bdc3424
rassafel <rassafel@gmail.com>
2022-03-15 15:54:54 +0400

    Add Orika config.

--- spring-boot-demo/src/main/java/ru/nshi/demo/config/OrikaConfig.java
+++ spring-boot-demo/src/main/java/ru/nshi/demo/config/OrikaConfig.java
@@ -1,24 +1,27 @@
 package ru.nshi.demo.config;
 
-import ma.glasnost.orika.CustomMapper;
+import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFactory;
-import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.converter.ConverterFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.nshi.demo.mapper.converter.StringToLocalDateConverter;
-import ru.nshi.demo.model.domain.Person;
-import ru.nshi.demo.model.dto.CreatePersonDto;
 
 /**
  * @author rassafel
  */
+@Slf4j
 @Configuration
+@EnableConfigurationProperties(OrikaProperties.class)
 public class OrikaConfig {
     @Bean
-    public MapperFactory mapperFactory() {
-        MapperFactory factory = new DefaultMapperFactory.Builder().build();
+    public MapperFactory mapperFactory(OrikaProperties properties) {
+        log.info("{}", properties);
+        MapperFactory factory = new DefaultMapperFactory.Builder()
+            .mapNulls(properties.getMapNulls())
+            .build();
 
         ConverterFactory converterFactory = factory.getConverterFactory();
         converterFactory.registerConverter(StringToLocalDateConverter.CONVERTER_CODE,
---
+++ spring-boot-demo/src/main/java/ru/nshi/demo/config/OrikaProperties.java
@@ -0,0 +1,14 @@
+package ru.nshi.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author rassafel
+ */
+@Data
+@ConfigurationProperties(prefix = "orika")
+public class OrikaProperties {
+   private Boolean mapNulls = false;
+   private String name;
+}
