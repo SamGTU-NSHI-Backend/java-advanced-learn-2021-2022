60939c454e6f5b583e3cb306fbf3be4b33c7ade9
rassafel <rassafel@gmail.com>
2022-03-22 13:59:45 +0400

    Add findAll.

--- spring-boot-demo/src/main/java/ru/nshi/demo/repository/PersonRepository.java
+++ spring-boot-demo/src/main/java/ru/nshi/demo/repository/PersonRepository.java
@@ -1,9 +1,13 @@
 package ru.nshi.demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import ru.nshi.demo.model.domain.Person;
+
+import java.util.List;
 
 /**
  * @author rassafel
  */
-public interface PersonRepository extends CrudRepository {
+public interface PersonRepository extends CrudRepository<Person, String> {
+    List<Person> findAll();
 }
